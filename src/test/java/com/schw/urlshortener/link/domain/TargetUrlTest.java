d554f710d5bf67cb59df03f55fbfb514c5a3a23f
Pamela Luis <pamelafluis@outlook.com>
2026-08-21 18:45:22 -0500

    Add failing tests for TargetUrl IP-literal blocked ranges (red)
    localhost, loopback, RFC1918 private ranges, and link-local (incl.
    169.254.169.254). Uses a resolver stub that throws if consulted, to
    assert IP-literal hosts are range-checked directly without a DNS hop.

--- src/test/java/com/schw/urlshortener/link/domain/TargetUrlTest.java
+++ src/test/java/com/schw/urlshortener/link/domain/TargetUrlTest.java
@@ -39,6 +39,58 @@ class TargetUrlTest {
 				.isThrownBy(() -> TargetUrl.of("https:///no-host", OWN_HOST, resolver));
 	}
 
+	@Test
+	void rejectsLocalhostRegardlessOfCase() {
+		HostnameResolver resolver = refusingResolver();
+
+		assertThatExceptionOfType(InvalidTargetUrlException.class)
+				.isThrownBy(() -> TargetUrl.of("http://localhost/x", OWN_HOST, resolver));
+		assertThatExceptionOfType(InvalidTargetUrlException.class)
+				.isThrownBy(() -> TargetUrl.of("http://LocalHost/x", OWN_HOST, resolver));
+	}
+
+	@Test
+	void rejectsLoopbackIpLiteralWithoutConsultingTheResolver() {
+		HostnameResolver resolver = refusingResolver();
+
+		assertThatExceptionOfType(InvalidTargetUrlException.class)
+				.isThrownBy(() -> TargetUrl.of("http://127.0.0.1/x", OWN_HOST, resolver));
+	}
+
+	@Test
+	void rejectsPrivateRangeIpLiterals() {
+		HostnameResolver resolver = refusingResolver();
+
+		for (String ip : new String[] {"10.0.0.5", "172.16.0.5", "192.168.1.5"}) {
+			assertThatExceptionOfType(InvalidTargetUrlException.class)
+					.describedAs("private IP literal '%s' should be rejected", ip)
+					.isThrownBy(() -> TargetUrl.of("http://" + ip + "/x", OWN_HOST, resolver));
+		}
+	}
+
+	@Test
+	void rejectsLinkLocalIpLiteralIncludingCloudMetadataAddress() {
+		HostnameResolver resolver = refusingResolver();
+
+		assertThatExceptionOfType(InvalidTargetUrlException.class)
+				.isThrownBy(() -> TargetUrl.of("http://169.254.169.254/x", OWN_HOST, resolver));
+	}
+
+	@Test
+	void acceptsAPublicIpLiteralWithoutConsultingTheResolver() {
+		HostnameResolver resolver = refusingResolver();
+
+		TargetUrl targetUrl = TargetUrl.of("http://93.184.216.34/x", OWN_HOST, resolver);
+
+		assertThat(targetUrl.value()).isEqualTo("http://93.184.216.34/x");
+	}
+
+	private static HostnameResolver refusingResolver() {
+		return host -> {
+			throw new AssertionError("resolver should not be consulted for an IP literal host: " + host);
+		};
+	}
+
 	private static HostnameResolver resolverReturning(String... literalIps) {
 		List<InetAddress> addresses = Arrays.stream(literalIps)
 				.map(TargetUrlTest::literalAddress)
